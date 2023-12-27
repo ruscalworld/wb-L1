3246a3e01ca511bb0c946fb261c13b5c92a2439a
Nikolay <me@ruscal.world>
2023-12-27 15:54:23 +0300

    Add comments

--- task11/main.go
+++ task11/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func Intersect[V comparable](a, b *Set[V]) *Set[V] {
 	result := NewSet[V]()
 
+	// Проходим по элементам множества a и добавляем в итоговое множество те их них,
+	// которые есть также в множестве b.
 	a.Walk(func(v V) {
 		if b.Has(v) {
 			result.Add(v)
@@ -15,10 +17,14 @@ func Intersect[V comparable](a, b *Set[V]) *Set[V] {
 }
 
 func main() {
+	// Создадим два множества, которые будут содержать целые числа от 1 до 10 и от 6 до 15.
 	a := NewSet(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-	b := NewSet(6, 7, 8, 9, 10, 11, 12, 13, 14)
+	b := NewSet(6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
 
+	// Находим пересечение двух множеств.
 	result := Intersect(a, b)
+
+	// Выводим элементы множества-пересечения.
 	result.Walk(func(v int) {
 		fmt.Println(v)
 	})
